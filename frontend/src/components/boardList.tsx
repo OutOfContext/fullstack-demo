d9a879043604f7a79d453dc94e326d9f1f648184
Raffael Hühnerschulte <raffael.huehnerschulte.ext@neuefische.de>
2024-03-29 12:17:11 +0100

    add drag and drop for dashboards

--- frontend/src/components/boardList.tsx
+++ frontend/src/components/boardList.tsx
@@ -1,7 +1,9 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import BoardComponent from "./board.tsx";
+import {DragDropContext, Droppable, Draggable, DragDropContextProps} from 'react-beautiful-dnd';
 import {Board} from "../types.ts";
+
 export default function BoardList() {
 
     const [boards, setBoards] = useState<Board[]>([])
@@ -16,13 +18,44 @@ export default function BoardList() {
             })
     }, []);
 
+    const onDragEnd = (result) => {
+        //console.log("DragEnd activated.")
+        if (!result.destination) return;
+
+        const newBoards = Array.from(boards);
+        const [reorderedBoard] = newBoards.splice(result.source.index, 1);
+        newBoards.splice(result.destination.index, 0, reorderedBoard);
+
+        setBoards(newBoards);
+    };
+
     return (
-        boards.length > 0 ? (
-            boards.map(board => (
-                <BoardComponent key={board.id} board={board}/>
-            ))
-        ) : (
-            <p>Keine Boards gefunden.</p>
-        )
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="boardlist" direction="vertical" type="ROW">
+                {(provided) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                        {boards.map((board, index) => (
+                            <Draggable
+                                key={board.id.toString()}
+                                index={index}
+                                draggableId={board.id.toString()}
+                            >
+                                {(provided) => (
+                                    <div
+                                        key={board.id.toString()}
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}
+                                    >
+                                        <BoardComponent board={board}/>
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     );
 }
